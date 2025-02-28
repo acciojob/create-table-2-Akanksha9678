ae1371e3519e57e57e7c219be44566fc58eb62b9
Akanksha Kushwaha <78742130+Akanksha9678@users.noreply.github.com>
2025-02-28 13:24:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 function createTable() {
     //Write your code here
-  
+  let tableSelect=document.querySelector("#myTable")
+  let inputBox=document.querySelector("input")
+	let rowNumber = parseInt(prompt("Input number of rows"));
+    let columnNumber = parseInt(prompt("Input number of columns"));
+
+	if(rowNumber<=0 && columnNumber!<=0){
+		for(let i=0;i<rowNumber;i++){
+		let row=document.createElement("tr")
+		table.append(tr)
+			for(let j=0;j<columnNumber;j++){
+			let column=document.createElement("td")
+			tr.append(td)
+			}
+		}
+	}
+
 }
